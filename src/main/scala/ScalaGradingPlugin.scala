dcc25b52491e98464131476a981caa5e737f1c9c
Yuvi Masory <ymasory@gmail.com>
2011-03-05 15:25:54 -0500

    [script commit]

---
+++ src/main/scala/ScalaGradingPlugin.scala
@@ -0,0 +1,35 @@
+package com.yuvimasory.scalagrading
+
+import scala.tools.nsc
+import nsc.Global
+import nsc.Phase
+import nsc.plugins.Plugin
+import nsc.plugins.PluginComponent
+
+class ScalaGrading(val global: Global) extends Plugin {
+  import global._
+
+  val name = "scala-grading"
+  val description = "grades functional style"
+  
+  val components = List[PluginComponent](ScalaGradingComponent)
+  
+  private object ScalaGradingComponent extends PluginComponent {
+    val global: ScalaGrading.this.global.type = ScalaGrading.this.global
+    val runsAfter = List[String]("refchecks");
+    val phaseName = ScalaGrading.this.name
+    def newPhase(_prev: Phase) = new SGNullPhase(_prev)
+    
+    class SGNullPhase(prev: Phase) extends StdPhase(prev) {
+      override def name = ScalaGrading.this.name
+      def apply(unit: CompilationUnit) {
+        for ( tree @ Apply(Select(rcvr, nme.DIV),
+                           List(Literal(Constant(0)))) <- unit.body;
+             if rcvr.tpe <:< definitions.IntClass.tpe) 
+          {
+            unit.error(tree.pos, "definitely division by zero")
+          }
+      }
+    }
+  }
+}
