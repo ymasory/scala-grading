84194024c28b8d8cadd1a4febdd82d5c8157da55
Yuvi Masory <ymasory@gmail.com>
2011-03-05 20:25:38 -0500

    prettify names

--- src/main/scala/ScalaGradingPlugin.scala
+++ src/main/scala/ScalaGradingPlugin.scala
@@ -24,11 +24,11 @@ class ScalaGrading(val global: Global) extends Plugin {
   private object SGNullComponent extends PluginComponent {
     override val global: ScalaGrading.this.global.type = ScalaGrading.this.global
     override val runsAfter = List[String]("refchecks");
-    override val phaseName = getClass.getName
+    override val phaseName = getClass.getName.split("\\$").last
     override def newPhase(_prev: Phase) = new SGNullPhase(_prev)
     
     class SGNullPhase(prev: Phase) extends StdPhase(prev) {
-      override def name = getClass.getName
+      override def name = getClass.getName.split("\\$").last
       override def apply(unit: CompilationUnit) {
         for (t @ DefDef(_, name, _, _, _, _) <- unit.body) {
           unit.warning(t.pos, "null def " + name + ", +1")
@@ -42,11 +42,11 @@ class ScalaGrading(val global: Global) extends Plugin {
   private object SGDefComponent extends PluginComponent {
     override val global: ScalaGrading.this.global.type = ScalaGrading.this.global
     override val runsAfter = List[String]("refchecks");
-    override val phaseName = getClass.getName
+    override val phaseName = getClass.getName.split("\\$").last
     override def newPhase(_prev: Phase) = new SGDefPhase(_prev)
     
     class SGDefPhase(prev: Phase) extends StdPhase(prev) {
-      override def name = getClass.getName
+      override def name = getClass.getName.split("\\$").last
       override def apply(unit: CompilationUnit) {
         for (t @ DefDef(_, name, _, _, _, _) <- unit.body) {
           unit.warning(t.pos, "function def " + name + ", +1")
