f9f3a6599ef978d27246c2dc87b70c4f0b26eece
Yuvi Masory <ymasory@gmail.com>
2011-03-06 01:31:28 -0500

    add execp func

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -20,7 +20,33 @@ class ScalaGradingTests extends FunSuite {
 
   test("one var") {
     expect(Score(0, 0, 0, 0, 1, 0, 0)) {
-      null
+      execp("ls")
     }
+  }
+
+  /** from scala-utilities, LGPL
+    * http://code.google.com/p/scala-utilities/
+    */
+  def execp (cmd : String) = {
+    import java.io._
+    var currDir : Option[File] = None
+    val runTime = Runtime.getRuntime
+    val process = if (currDir.isDefined) runTime.exec (cmd, null, currDir.get) else runTime.exec(cmd)
+    val resultBuffer = new BufferedReader(new InputStreamReader(process.getInputStream))
+    var line : String = null
+    var lineList : List[String] = Nil
+
+
+    do {
+        line = resultBuffer.readLine
+        if (line != null) {
+            lineList = line :: lineList
+        }
+    } while (line != null)
+
+    process.waitFor
+    resultBuffer.close
+
+    (process.exitValue, lineList.reverse)
   }
 }
