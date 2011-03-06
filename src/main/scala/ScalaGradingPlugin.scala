4b626e67b07ed4145a39beba8f25807face5c46e
Yuvi Masory <ymasory@gmail.com>
2011-03-05 20:52:14 -0500

    add multiple phase scaffolding

--- src/main/scala/ScalaGradingPlugin.scala
+++ src/main/scala/ScalaGradingPlugin.scala
@@ -15,44 +15,44 @@ class ScalaGrading(val global: Global) extends Plugin {
   override val name = "scala-grading"
   override val description = "grades functional style"
   
-  override val components = List[PluginComponent] (
+  override val components = List (
     SGDefComponent,
     SGNullComponent
   )
   
 
-  private object SGNullComponent extends PluginComponent {
+  protected abstract class SGAbstractComponent extends PluginComponent {
     override val global: ScalaGrading.this.global.type = ScalaGrading.this.global
     override val runsAfter = List[String]("refchecks");
     override val phaseName = getClass.getName.split("\\$").last
+  }
+
+  object SGNullComponent extends SGAbstractComponent {
     override def newPhase(_prev: Phase) = new SGNullPhase(_prev)
     
     class SGNullPhase(prev: Phase) extends StdPhase(prev) {
-      override def name = getClass.getName.split("\\$").last
       override def apply(unit: CompilationUnit) {
         for (t @ DefDef(_, name, _, _, _, _) <- unit.body) {
-          unit.warning(t.pos, "null def " + name + ", +1")
+          global.reporter.info(t.pos, "null def " + name + ", +1", true)
           bonusPoints += 1
         }
       }
     }
   }
 
-
-  private object SGDefComponent extends PluginComponent {
-    override val global: ScalaGrading.this.global.type = ScalaGrading.this.global
-    override val runsAfter = List[String]("refchecks");
-    override val phaseName = getClass.getName.split("\\$").last
+  object SGDefComponent extends SGAbstractComponent {
     override def newPhase(_prev: Phase) = new SGDefPhase(_prev)
     
     class SGDefPhase(prev: Phase) extends StdPhase(prev) {
       override def name = getClass.getName.split("\\$").last
       override def apply(unit: CompilationUnit) {
         for (t @ DefDef(_, name, _, _, _, _) <- unit.body) {
-          unit.warning(t.pos, "function def " + name + ", +1")
+          global.reporter.info(t.pos, "null def " + name + ", +1", true)
           bonusPoints += 1
         }
       }
     }
   }
+
+
 }
