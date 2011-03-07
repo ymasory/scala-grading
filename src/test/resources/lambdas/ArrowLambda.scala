a134dbb86896a2e408be2e2755e8a9a114acb9e0
Yuvi Masory <ymasory@gmail.com>
2011-03-06 19:35:26 -0500

    add lambda arrow test

---
+++ src/test/resources/lambdas/ArrowLambda.scala
@@ -0,0 +1,4 @@
+object ArrowLambda {
+
+  val max = {(x: Int, y: Int) => if (x > y) x else y}
+}
---
+++ src/test/scala/LambdaTests.scala
@@ -0,0 +1,18 @@
+package com.yuvimasory.scalagrading
+
+import org.scalatest.FunSuite
+
+class LambdaTests extends FunSuite {
+
+  test("ArrowLambda") {
+    val expected = Score(defs=0,
+                         lambdas=1,
+                         matches=0,
+                         deceptions=0,
+                         whiles=0,
+                         vars=0,
+                         arrays=0,
+                         nulls=0)
+    expect(expected) {Score.runPlugin("lambdas/ArrowLambda.scala")}
+  }
+}
