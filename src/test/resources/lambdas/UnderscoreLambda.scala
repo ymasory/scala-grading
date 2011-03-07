6f43ab828246811a9e2012ab9a1d7d803e3fc730
Yuvi Masory <ymasory@gmail.com>
2011-03-06 19:43:17 -0500

    pass underscore lambda test

---
+++ src/test/resources/lambdas/UnderscoreLambda.scala
@@ -0,0 +1,4 @@
+object ArrowLambda {
+
+  val sum = List(1, 2, 3).foldLeft(0)(_ + _)
+}
--- src/test/scala/LambdaTests.scala
+++ src/test/scala/LambdaTests.scala
@@ -15,4 +15,16 @@ class LambdaTests extends FunSuite {
                          nulls=0)
     expect(expected) {Score.runPlugin("lambdas/ArrowLambda.scala")}
   }
+
+  test("UnderscoreLambda") {
+    val expected = Score(defs=0,
+                         lambdas=1,
+                         matches=0,
+                         deceptions=0,
+                         whiles=0,
+                         vars=0,
+                         arrays=0,
+                         nulls=0)
+    expect(expected) {Score.runPlugin("lambdas/UnderscoreLambda.scala")}
+  }
 }
